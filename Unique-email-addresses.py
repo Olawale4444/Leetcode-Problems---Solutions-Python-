784b3773f295362f8b55f408105099a58e1d4d9a
Wale Olowonyo F <31905212+wale1454@users.noreply.github.com>
2022-09-03 12:32:13 +0100

    Create Unique-email-addresses.py

---
+++ Unique-email-addresses.py
@@ -0,0 +1,24 @@
+#https://leetcode.com/problems/unique-email-addresses/
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        
+        SampleSet = set()
+        
+        for j in emails:
+            first, last = j.split("@")
+            drift = ""
+            
+            for i in first:
+                if i== ".":
+                    continue
+
+                if i =="+":
+                    break
+                
+                drift += i
+
+            SampleSet.add(drift+ "@" + last)
+
+        return len(SampleSet)
+        
